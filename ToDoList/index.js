9c4abf4c3a941d1255719b8944044d9262e86930
EnQin <fm06184cj3@gmail.com>
2021-11-13 20:12:44 +0800

    第一個最後修正版(周六

--- ToDoList/index.js
+++ ToDoList/index.js
@@ -1,55 +1,41 @@
-const whichPage = document.getElementById("whichPage").innerText;
 let count = document.getElementById("count").innerText;
 let completed = document.getElementById("completed").innerText;
 const cpPage = document.getElementById("cpPage").innerText;
-console.log(`目前在第${whichPage}頁`);
 const record = document.getElementById("record").innerText;
-const add = document.getElementById("add").value;
-// window.onload = function () {
-//   fetch("/onloadData", {
-//     method: "DELETE",
-//     body: JSON.stringify({
-//       WhichPage: whichPage,
-//       Number: "1",
-//       cpPage: cpPage,
-//     }),
-//   })
-//     .then((response) => {
-//       console.log(response);
-//       return response.json();
-//     })
-//     .then((response) => console.log(response));
-// };
+
+function addHandler(x) {
+  let myForm = document.getElementById("myForm");
+  let formData = new FormData(myForm);
+  fetch("/add", {
+    method: "POST",
+    body: formData,
+  }).then(() => {
+    window.location.href = `/ncp/1/${x}`;
+  });
+}
 
 let pages;
+let pageURL;
 if (cpPage === "false") {
   if (count === "0") {
     count = 1;
   }
-  pages = Math.floor((count - 1) / 4) + 1;
+  pages = Math.floor((count - 1) / record) + 1;
+  pageURL = "ncp";
 } else {
   if (completed === "0") {
     completed = 1;
   }
-  pages = Math.floor((completed - 1) / 4) + 1;
+  pages = Math.floor((completed - 1) / record) + 1;
+  pageURL = "cp";
 }
 
-if (cpPage === "false") {
-  for (i = 0; i < pages; i++) {
-    const a = document.createElement("a");
-    a.innerText = i + 1;
-    a.href = `/ncp/${i + 1}/${record}`;
-    a.style = "margin:3px";
-    document.getElementById("pageDiv").appendChild(a);
-  }
-} else if (cpPage === "true") {
-  for (i = 0; i < pages; i++) {
-    const a = document.createElement("a");
-    a.innerText = i + 1;
-    a.href = `/cp/${i + 1}/${record}`;
-    a.style = "margin:3px";
-    document.getElementById("pageDiv").appendChild(a);
-  }
+for (i = 0; i < pages; i++) {
+  const a = document.createElement("a");
+  a.innerText = i + 1;
+  a.href = `/${pageURL}/${i + 1}/${record}`;
+  a.style = "margin:3px";
+  document.getElementById("pageDiv").appendChild(a);
 }
 
 function toggleHandler(x) {
@@ -57,82 +43,48 @@ function toggleHandler(x) {
     method: "PUT",
     body: JSON.stringify({
       Id: x,
+      cpPage,
     }),
     headers: {
       "content-type": "application/json",
     },
   }).then(() => {
-    if (cpPage === "false") {
-      window.location.href = `/ncp/1/${record}`;
-    } else if (cpPage === "true") {
-      window.location.href = `/cp/1/${record}`;
-    }
+    window.location.href = `/${pageURL}/1/${record}`;
   });
 }
 
 function updateHandler(x) {
   const newName = document.getElementById(x).value;
-  fetch(`/${whichPage}`, {
+  fetch("/changeName", {
     method: "PUT",
     body: JSON.stringify({
-      WhichPage: whichPage,
-      Number: x,
+      Id: x,
       NewName: newName,
-      cpPage: cpPage,
     }),
     headers: {
       "content-type": "application/json",
     },
   }).then(() => {
-    if (cpPage === "false") {
-      window.location.href = `/${whichPage}`;
-    } else {
-      window.location.href = `/completed/${whichPage}`;
-    }
+    window.location.href = `/${pageURL}/1/${record}`;
   });
 }
 
 function deleteHandler(x) {
-  fetch("/", {
+  fetch("/deleteTodo", {
     method: "DELETE",
     body: JSON.stringify({
-      WhichPage: whichPage,
-      Number: `${x}`,
-      cpPage: cpPage,
+      Id: x,
     }),
     headers: {
       "content-type": "application/json",
     },
   }).then(() => {
-    window.location.href = "/1";
-  });
-}
-
-function addHandler(record) {
-  console.log(record)
-  fetch("/add", {
-    method: "POST",
-    body: JSON.stringify({
-      Name: "gggg" 
-    }),
-    headers: {
-      "content-type": "application/json",
-    },
-  }).then(() => {
-    window.location.href = `/ncp/1/${record}`;
+    window.location.href = `/${pageURL}/1/${record}`;
   });
 }
 
 function recordHandler(x) {
-  if (typeof x === "number") {
-    if (x <= 0 || x % 1 !== 0) {
-      alert("資料筆數需為正整數");
-      return;
-    }
-    if (cpPage === "false") {
-      window.location.href = `/ncp/1/${x}`;
-    } else if (cpPage === "true") {
-      window.location.href = `/cp/1/${x}`;
-    }
+  if (x > 0 && x % 1 === 0) {
+    window.location.href = `/${pageURL}/1/${x}`;
   }
 }
--- ToDoList/main.go
+++ ToDoList/main.go
@@ -12,7 +12,7 @@ import (
 )
 
 func main() {
-	dsn := "root:root1234@tcp(127.0.0.1:13306)/db1?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:root1234@tcp(127.0.0.1:3306)/db1?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Println("連上mysql報錯:", err)
@@ -23,65 +23,10 @@ func main() {
 		log.Println("database做AuToMigrate報錯", err)
 		return
 	}
-	fmt.Println("伺服器開啟")
 	r := gin.Default()
 	r.LoadHTMLGlob("./*")
 	r.Static("/ToDoList", "./")
 
-	// r.GET("/:page", func(c *gin.Context) { //連到未完成事項頁面
-	// 	var count int64     //未完成數
-	// 	var completed int64 //已完成數
-
-	// 	if err := db.Debug().Table("todos").Where("completed", false).Where("deleted_at", nil).Count(&count).Error; err != nil {
-	// 		log.Println("未完成事項的頁面撈未完成數count報錯", err)
-	// 		c.Redirect(http.StatusMovedPermanently, "/1")
-	// 		return
-	// 	}
-
-	// 	if err := db.Debug().Table("todos").Where("completed", true).Where("deleted_at", nil).Count(&completed).Error; err != nil {
-	// 		log.Println("未完成事項頁面 撈已完成事項數量報錯", err)
-	// 		c.Redirect(http.StatusMovedPermanently, "/1")
-	// 		return
-	// 	}
-
-	// 	page := c.Param("page")
-
-	// 	i, err := strconv.Atoi(page)
-	// 	if err != nil {
-	// 		log.Println("page做Atoi時報錯", err)
-	// 		c.Redirect(http.StatusMovedPermanently, "/1")
-	// 		return
-	// 	}
-	// 	T := make([]Todo, 4)
-	// 	fmt.Printf("T: %v\n", T)
-	// 	if count%4 != 0 && i > int(count/4) {
-	// 		// T2 := []Todo{}
-	// 		if err = db.Table("todos").Where("completed", false).Offset(i*4 - 4).Limit(4).Find(&T).Error; err != nil {
-	// 			log.Println("未完成事項拿1~3筆的資料報錯", err)
-	// 			return
-	// 		}
-
-	// 		// for c, v := range T {
-	// 		// 	fmt.Printf("v: %d = %v\n", c, v)
-	// 		// }
-	// 		// for c, v := range T2 {
-	// 		// 	fmt.Printf("T2: %d = %v\n", c, v)
-	// 		// }
-	// 		// copy(T, T2)
-	// 		// for c, v := range T {
-	// 		// 	fmt.Printf("T1: %d = %v\n", c, v)
-	// 		// }
-	// 	} else {
-	// 		if err = db.Table("todos").Where("completed", false).Offset(i*4 - 4).Limit(4).Find(&T).Error; err != nil {
-	// 			log.Println("未完成事項拿四筆整的資料報錯", err)
-	// 			return
-	// 		}
-	// 	}
-	// 	//
-
-	// 	c.HTML(http.StatusOK, "index.html",
-	// 		gin.H{"T": T, "page": page, "count": count, "completed": completed, "cpPage": false})
-	// })
 	r.GET("/:cpPage/:page/:record", func(c *gin.Context) { //連到已完成事項頁面
 		var count int64     //未完成數
 		var completed int64 //已完成數
@@ -94,13 +39,13 @@ func main() {
 		}
 		if err := db.Debug().Table("todos").Where("completed", false).Where("deleted_at", nil).Count(&count).Error; err != nil {
 			log.Println("未完成事項的頁面撈未完成數count報錯", err)
-			c.Redirect(http.StatusMovedPermanently, "/1")
+			c.Redirect(http.StatusMovedPermanently, "/ncp/1/4")
 			return
 		}
 
 		if err := db.Debug().Table("todos").Where("completed", true).Where("deleted_at", nil).Count(&completed).Error; err != nil {
 			log.Println("未完成事項頁面 撈已完成事項數量報錯", err)
-			c.Redirect(http.StatusMovedPermanently, "/1")
+			c.Redirect(http.StatusMovedPermanently, "/ncp/1/4")
 			return
 		}
 
@@ -109,29 +54,28 @@ func main() {
 		i, err := strconv.Atoi(page)
 		if err != nil {
 			log.Println("page做Atoi時報錯", err)
-			c.Redirect(http.StatusMovedPermanently, "/1")
+			c.Redirect(http.StatusMovedPermanently, "/ncp/1/4")
 			return
 		}
 		T := []Todo{}
-		fmt.Printf("T: %v\n", T)
 
 		if err = db.Table("todos").Where("completed", cpPage).Offset(record * (i - 1)).Limit(record).Find(&T).Error; err != nil {
-			log.Println("getHandler中拿資料報錯", err)
+			log.Println("GET路由dB拿資料報錯", err)
+			c.Redirect(http.StatusMovedPermanently, "/ncp/1/4")
 			return
 		}
 
-		c.HTML(http.StatusOK, "index.tmpl",
+		c.HTML(http.StatusOK, "index.html",
 			gin.H{"T": T, "page": i, "count": count, "completed": completed, "cpPage": cpPage, "record": record})
 	})
 	r.POST("/add", func(c *gin.Context) { // 添加事項
-		var t Todo
-		if err := c.BindJSON(&t); err != nil {
-			log.Println("新增事項的BindJSON報錯", err)
+		name := c.PostForm("add")
+
+		fmt.Println("新增:", name)
+		if name == "" {
 			return
 		}
-		fmt.Printf("%+v", t)
-		fmt.Println("新增:", t.Name)
-		if err := db.Create(&Todo{gorm.Model{}, t.Name, false}).Error; err != nil {
+		if err := db.Create(&Todo{gorm.Model{}, name, false}).Error; err != nil {
 			log.Println("建立事件db.Create時報錯", err)
 			return
 		}
@@ -139,99 +83,66 @@ func main() {
 	r.PUT("/changeName", func(c *gin.Context) { //改名
 		var json UdJson
 		if err := c.BindJSON(&json); err != nil {
-			log.Println("事項做改名的BindJSON報錯", err)
+			log.Println("事項做改名的BindJSON報錯:", err)
 			return
 		}
-		fmt.Printf("%+v\n", json)
-		// WhichPage, err := strconv.Atoi(json.WhichPage)
-		// if err != nil {
-		// 	log.Println("json.WhichPage做Atoi時報錯", err)
-		// 	return
-		// }
-		// Number, err := strconv.Atoi(json.Number)
-		// if err != nil {
-		// 	log.Println("json.Number做Atoi時報錯", err)
-		// 	return
-		// }
-		// var cpPage bool = json.CpPage == "true"
-		// fmt.Println(cpPage)
-
+		fmt.Printf("改名收到了json:%+v\n", json)
+		if json.NewName == "" {
+			return
+		}
 		var t Todo
-		db.First(&t, json.Id)
-		if t.Name != "" {
-			db.Model(&t).Update("name", json.NewName)
+		if err := db.First(&t, json.Id).Error; err != nil {
+			log.Println("dB找出要改名之項目的First函數報錯:", err)
+			return
+		}
+		if err := db.Model(&t).Update("name", json.NewName).Error; err != nil {
+			log.Println("dB做項目改名的Update函數報錯:", err)
+			return
 		}
 	})
 
 	r.PUT("/changeState", func(c *gin.Context) { //改狀態
 		var json UdJson
 		if err := c.BindJSON(&json); err != nil {
-			log.Println("事項切換完成狀態的BindJSON報錯", err)
+			log.Println("事項切換完成狀態的BindJSON報錯:", err)
 			return
 		}
-		fmt.Printf("%+v\n", json)
-		WhichPage, err := strconv.Atoi(json.WhichPage)
-		if err != nil {
-			log.Println("json.WhichPage做Atoi時報錯", err)
-			return
-		}
-		Number, err := strconv.Atoi(json.Number)
-		if err != nil {
-			log.Println("json.Number做Atoi時報錯", err)
-			return
-		}
+		fmt.Printf("切換完成狀態 json:%+v\n", json)
 
-		var cpPage bool
-		fmt.Println(cpPage)
-		if json.CpPage == "false" {
-			cpPage = false
-			fmt.Println(cpPage)
-		} else if json.CpPage == "true" {
-			cpPage = true
-			fmt.Println(cpPage)
-		}
-		fmt.Println(cpPage)
+		var cpPage bool = json.CpPage == "true"
+
 		var t Todo
-		if err := db.Where("completed", cpPage).Offset(((WhichPage)-1)*4 + Number - 1).First(&t).Error; err != nil {
-			log.Println("dB找出要改哪一筆資料的完成狀態時報錯", err)
+		if err := db.First(&t, json.Id).Error; err != nil {
+			log.Println("dB找出要改哪一筆事件的完成狀態時First函數報錯:", err)
 			return
 		}
 		if err := db.Model(&t).Update("completed", !cpPage).Error; err != nil {
-			log.Println("dB改資料的完成狀態時報錯", err)
+			log.Println("dB改資料的完成狀態時Update函數報錯:", err)
 			return
 		}
 	})
 
-	r.DELETE("/", func(c *gin.Context) { //刪除項目
+	r.DELETE("/deleteTodo", func(c *gin.Context) { //刪除項目
 		var json UdJson
 		if err := c.BindJSON(&json); err != nil {
-			log.Println("刪除未完成項目的路由中BindJson報錯", err)
+			log.Println("項目刪除的路由中BindJson報錯", err)
+			return
 		}
-		fmt.Printf("%#v\n", json)
-
-		WhichPage, err := strconv.Atoi(json.WhichPage)
-		if err != nil {
-			log.Println("json.WhichPage做Atoi時報錯", err)
-		}
-		Number, err := strconv.Atoi(json.Number)
-		if err != nil {
-			log.Println("json.Number做Atoi時報錯", err)
-		}
-
-		var cpPage bool
-		if json.CpPage == "false" {
-			cpPage = false
-		} else if json.CpPage == "true" {
-			cpPage = true
-		}
+		fmt.Printf("事件刪除路由json:%#v\n", json)
 
 		var t Todo
-		db.Where("completed", cpPage).Offset(((WhichPage)-1)*4 + Number - 1).First(&t)
-		db.Debug().Delete(&t)
+		if err := db.Debug().First(&t, json.Id).Error; err != nil {
+			log.Println("dB找出要刪除的todo時報錯:", err)
+			return
+		}
+		if err := db.Debug().Delete(&t).Error; err != nil {
+			log.Println("dB刪除todo時報錯:", err)
+			return
+		}
 	})
 
 	r.NoRoute(func(c *gin.Context) {
-		fmt.Println("NoRoute已作動")
+		fmt.Println("NoRoute已作動   重定向至 /ncp/1/4")
 		c.Redirect(http.StatusMovedPermanently, "/ncp/1/4")
 	})
 
@@ -248,10 +159,7 @@ type Todo struct {
 }
 
 type UdJson struct {
-	WhichPage string
-	Number    string
-	NewName   string
-	CpPage    string
-	Id        uint
-	Record    uint
+	NewName string
+	CpPage  string
+	Id      uint
 }
